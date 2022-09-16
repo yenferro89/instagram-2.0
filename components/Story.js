2dd84bcb7e6f4f30fda2abd33d0d528dd9eb4de6
Yen Pinero Ferro <ypine022@fiu.edu>
2022-09-16 15:14:55 -0400

    commit on 'main'

--- components/Story.js
+++ components/Story.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Story({ img, username }) {
   return (
-    <div className=''>
-        <img src={ img } alt="" />
-        <p>{ username }</p>
+    <div className="">
+      <img
+        className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain 
+        cursor-pointer hover:scale-110 transition transform duration-200 ease-out"
+        src={img}
+        alt=""
+      />
+      <p className="text-xs w-14 truncate text-center">{username}</p>
     </div>
-  )
+  );
 }
 
-export default Story
+export default Story;
