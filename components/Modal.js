022d68e60745726c028dc6a3cbb1a221de098386
Yen Pinero Ferro <ypine022@fiu.edu>
2022-09-23 22:14:53 -0400

    commit on 'Modal'

--- components/Modal.js
+++ components/Modal.js
@@ -1,22 +1,33 @@
-import { Dialog, Transition } from '@headlessui/react'
-import React, { Fragment } from 'react'
-import { useRecoilState } from 'recoil'
-import { modalAtom } from '../atoms/modalAtom'
+import { Dialog, Transition } from "@headlessui/react";
+import React, { Fragment } from "react";
+import { useRecoilState } from "recoil";
+import { modalAtom } from "../atoms/modalAtom";
 
 function Modal() {
-  const [open, setOpen]= useRecoilState(modalAtom)
+  const [open, setOpen] = useRecoilState(modalAtom);
 
   return (
     <Transition.Root show={open} as={Fragment}>
-        <Dialog
-            as='div'
-            className='fixed z-10 inset-0 overflow-y-auto'
-            onClose={setOpen}
-        >
-            <h1> Hello Dialoguete</h1>
-        </Dialog>    
+      <Dialog
+        as="div"
+        className="fixed z-10 inset-0 overflow-y-auto"
+        onClose={setOpen}
+      >
+        <div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center">
+            <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0">
+                    <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+            </Transition.Child>
+        </div>
+      </Dialog>
     </Transition.Root>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
