68400c2232fdba03ecebdb2d3e9625b6c173c1cc
Yen Pinero Ferro <ypine022@fiu.edu>
2022-09-26 11:14:34 -0400

    commit on 'Modal"

--- components/Modal.js
+++ components/Modal.js
@@ -1,11 +1,66 @@
 import { Dialog, Transition } from "@headlessui/react";
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalAtom } from "../atoms/modalAtom";
 import { CameraIcon } from "@heroicons/react/24/outline";
+import { db, storage } from "../firebase";
+import {
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { ref, getDownloadURL, uploadString } from "firebase/storage";
 
 function Modal() {
+  const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalAtom);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const captionRef = useRef(null);
+  const filePickerRef = useRef(null);
+
+  const uploadPost = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, "posts"), {
+      username: session.user.username,
+      caption: captionRef.current.value,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    console.log("New doc added with ID", docRef.id);
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    await uploadString(imageRef, selectedFile, "data_url").then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadURL,
+        });
+      }
+    );
+
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -41,17 +96,26 @@ function Modal() {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div
-              className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden 
-            shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
-            >
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
               <div>
-                <div className="flex mx-auto items-center justify-center h-12 w-12 rounded-full bg-red-100">
-                  <CameraIcon
-                    className="h-6 w-6 text-red-600"
-                    aria-hidden="true"
+                {selectedFile ? (
+                  <img
+                    src={selectedFile}
+                    className=" w-full object-contain cursor-pointer"
+                    onClick={() => setSelectedFile(null)}
                   />
-                </div>
+                ) : (
+                  <div
+                    onClick={() => filePickerRef.current.click()}
+                    className="flex mx-auto items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                  >
+                    <CameraIcon
+                      className="h-6 w-6 text-red-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                )}
+
                 <div className="mt-3 text-center sm:mt-5">
                   <Dialog.Title
                     as="h3"
@@ -60,11 +124,17 @@ function Modal() {
                     Upload a photo
                   </Dialog.Title>
                   <div>
-                    <input type="file" hidden />
+                    <input
+                      type="file"
+                      hidden
+                      ref={filePickerRef}
+                      onChange={addImageToPost}
+                    />
                   </div>
                   <div className="mt-2">
                     <input
                       className="border-none focus:ring-0 w-full text-center"
+                      ref={captionRef}
                       type="text"
                       placeholder="Please enter a caption..."
                     />
@@ -73,12 +143,14 @@ function Modal() {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
+                    disabled={!selectedFile}
                     className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm
                               px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none
                               focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300
                               disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                    onClick={uploadPost}
                   >
-                    Upload Post
+                    {loading ? "Uploading..." : "Upload post"}
                   </button>
                 </div>
               </div>
