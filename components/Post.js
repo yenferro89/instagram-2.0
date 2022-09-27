f9cf78ad6f1d7914264137484cfa168eaf391d3f
Yen Pinero Ferro <ypine022@fiu.edu>
2022-09-27 14:26:23 -0400

    commit on 'Post'

--- components/Post.js
+++ components/Post.js
@@ -116,6 +116,9 @@ function Post({ id, username, userImg, img, caption }) {
 
       {/* Captions */}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} likes</p>
+        )}
         <span className=" font-bold mr-1">{username}</span>
         {caption}
       </p>
